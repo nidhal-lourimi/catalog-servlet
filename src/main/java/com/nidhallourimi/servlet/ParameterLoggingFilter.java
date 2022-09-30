f199cee39546e3edaa757aebca0c7fe14a9b5acb
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-09-30 20:39:01 +0100

    adding filter to extract client entry

---
+++ src/main/java/com/nidhallourimi/servlet/ParameterLoggingFilter.java
@@ -0,0 +1,23 @@
+package com.nidhallourimi.servlet;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter("/catalog-servlet")
+public class ParameterLoggingFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.getParameterMap().entrySet().stream().forEach(entry ->{
+            System.out.println(String.format("%s:%s", entry.getKey(), entry.getValue()[0]));
+        });
+        chain.doFilter(request, response);
+    }
+
+}
